ff100f7219915eba42af5f247e4c6ffa46b7349c
kalesnehal16 <kalesnehal16@gmail.com>
2024-12-21 20:58:08 +0530

    examples for BD1.2

--- index.js
+++ index.js
@@ -18,10 +18,38 @@ app.get('/total-time', (req, res) => {
   let time1 = parseFloat(req.query.time1);
   let time2 = parseFloat(req.query.time2);
   let time3 = parseFloat(req.query.time3);
-  let totalTime = time1 + time2 +time3;
+  let totalTime = time1 + time2 + time3;
   res.send(totalTime.toString());
+});
 
-  // res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get('/average-speed', (req, res) => {
+  let totalDistance = parseFloat(req.query.totalDistance);
+  let totalTime = parseFloat(req.query.totalTime);
+  let averageSpeed = totalDistance / totalTime;
+  res.send(averageSpeed.toString());
+});
+
+app.get('/eta', (req, res) => {
+  let distance = parseFloat(req.query.distance);
+  let speed = parseFloat(req.query.speed);
+  let eta = distance / speed;
+  res.send(eta.toString());
+});
+
+app.get('/total-calories', (req, res) => {
+  let duration1 = parseFloat(req.query.duration1);
+  let duration2 = parseFloat(req.query.duration2);
+  let caloriesPerMin = parseFloat(req.query.caloriesPerMinute);
+  let totalCalories = (duration1 + duration2) * caloriesPerMin;
+  res.send(totalCalories.toString());
+});
+
+app.get('/interest-earned', (req, res) => {
+  let principal = parseFloat(req.query.principal);
+  let rate = parseFloat(req.query.rate);
+  let time = parseFloat(req.query.time);
+  let interestEarned = (principal * rate * time) / 100;
+  res.send(interestEarned.toString());
 });
 
 app.listen(port, () => {
