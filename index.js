8974c2a140ce4054102925940df1a91f051b6295
kalesnehal16 <git@stackblitz.com>
2024-12-21 18:52:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+
+app.get('/total-distance', (req, res) => {
+  let distance1 = parseFloat(req.query.distance1);
+  let distance2 = parseFloat(req.query.distance2);
+  let totalDistance = distance1 + distance2;
+  res.send(totalDistance.toString());
+
+  // res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
