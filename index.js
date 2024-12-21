fc7936f44b004adf5fe59c69085268198d3dda9a
kalesnehal16 <kalesnehal16@gmail.com>
2024-12-21 20:04:37 +0530

    checkTotalTime

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/total-distance', (req, res) => {
   let distance2 = parseFloat(req.query.distance2);
   let totalDistance = distance1 + distance2;
   res.send(totalDistance.toString());
+  // res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/total-time', (req, res) => {
+  let time1 = parseFloat(req.query.time1);
+  let time2 = parseFloat(req.query.time2);
+  let time3 = parseFloat(req.query.time3);
+  let totalTime = time1 + time2 +time3;
+  res.send(totalTime.toString());
 
   // res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
